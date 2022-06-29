a77a0dbbdf364719387fe86652a0a7b7aaa0ad59
Arthur Vrt <46626919+ArthurVEROT@users.noreply.github.com>
2022-06-29 17:25:18 +0200

    user routes done

---
+++ routes/user.routes.js
@@ -0,0 +1,39 @@
+const router = require("express").Router();
+// const isAuthenticated = require("../middleware/isAuthenticated");
+const User = require("../models/User.model");
+
+// Get user info
+router.get("/user", async (req, res, next) => {
+  try {
+    const id = req.user._id;
+    const userInfo = await User.findById(id);
+    res.status(200).json(userInfo);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Modify user profile
+router.patch("/user", async (req, res, next) => {
+  try {
+    const id = req.user._id;
+    const update = req.body;
+    const updatedUser = await User.findByIdAndUpdate(id, update);
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Delete user profile
+router.delete("/user", async (req, res, next) => {
+  try {
+    const id = req.user._id;
+    await User.findByIdAndDelete(id);
+    res.status(204)
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
