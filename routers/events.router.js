3538f2c1cd2eba0647726b32f62506b9e26df08c
Haroun B <haroun@harryb.dev>
2022-07-01 15:51:04 +0200

    add edit event by id

--- routers/events.router.js
+++ routers/events.router.js
@@ -8,7 +8,7 @@ const Event = require("../models/Event.model");
 // get events based on filter values
 router.get(`/`, async (req, res, next) => {
   try {
-    
+
   } catch (err) {
     next(err);
   }
@@ -67,7 +67,58 @@ router.post("/", async (req, res, next) => {
 // edit event by id
 router.patch(`/:eventId`, async (req, res, next) => {
   try {
+    const { eventId } = req.params;
 
+    const foundEvent = await Event.findById(eventId)
+      .populate({ path: `creator`, select: { password: 0, email: 0, __v: 0 } });
+
+    if (!foundEvent) {
+      handleNotExist(`event`, eventId, res);
+      return;
+    }
+
+    if (foundEvent.creator.id !== req.user.id) {
+      res.status(403)
+        .json({
+          errors: {
+            event: `cannot edit an event you didn't create`
+          }
+        });
+      return;
+    }
+
+    let {
+      title,
+      location,
+      startAt,
+      endAt,
+      attendees,
+      description,
+      type,
+      approvalRequired,
+    } = req.body;
+
+    startAt = new Date(startAt);
+    endAt = new Date(endAt);
+
+    const updatedEvent = await Event.findByIdAndUpdate(
+      eventId,
+      {
+        creator: req.user.id,
+        title,
+        location,
+        startAt,
+        endAt,
+        attendees,
+        description,
+        type,
+        approvalRequired,
+      },
+      { runValidators: true, new: true }
+    ).populate({ path: `creator`, select: { password: 0, email: 0, __v: 0 } });
+
+
+    res.status(200).json(updatedEvent);
   } catch (err) {
     next(err);
   }
