1f907c406813c85de697cc17891a2b5cd056d1e5
Haroun B <haroun@harryb.dev>
2022-07-03 12:27:19 +0200

    adds messageId and eventId to MessageReadReceipt.model

--- models/MessageReadReceipt.model.js
+++ models/MessageReadReceipt.model.js
@@ -1,35 +1,39 @@
 const { Schema, model } = require(`mongoose`);
 
-const messageReceiptSchema = new Schema(
-  {
-    received: [
-      {
-        by: {
-          type: Schema.Types.ObjectId,
-          ref: `User`,
-          required: true
-        },
-        at: {
-          type: Date,
-          required: true
-        }
-      }
-    ],
-    seen: [
-      {
-        by: {
-          type: Schema.Types.ObjectId,
-          ref: `User`,
-          required: true
-        },
-        at: {
-          type: Date,
-          required: true
-        }
-      }
-    ]
+const messageReceiptSchema = new Schema({
+  event: {
+    type: Schema.Types.ObjectId,
+    ref: `Event`,
+    required: true
+  },
+  message: {
+    type: Schema.Types.ObjectId,
+    ref: `Message`,
+    required: true
+  },
+  received: {
+    by: {
+      type: Schema.Types.ObjectId,
+      ref: `User`,
+      required: true
+    },
+    at: {
+      type: Date,
+      required: true
+    }
+  },
+  seen: {
+    by: {
+      type: Schema.Types.ObjectId,
+      ref: `User`,
+      required: true
+    },
+    at: {
+      type: Date,
+      required: true
+    }
   }
-)
+})
 
 // TODO: add static methods: makeReceived; makeSeen
 
